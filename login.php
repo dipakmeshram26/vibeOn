65d7045c775980d8a6b63a20eaac84d38072e3ac
Dipak meshram <meshramdm2@gmail.com>
2025-09-11 23:50:25 +0530

    login page
    styling of login page

--- login.php
+++ login.php
@@ -7,21 +7,29 @@ if (isset($_SESSION['user_id'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>VibeOn - Login</title>
+    <link rel="stylesheet" href="homestyle.css">
 </head>
+
 <body>
-    <h2>Login to VibeOn</h2>
-    <form action="login_process.php" method="POST">
-        <label>Username or Email:</label>
-        <input type="text" name="login_id" required><br><br>
+    <div class="log-cont">
 
-        <label>Password:</label>
-        <input type="password" name="password" required><br><br>
 
-        <button type="submit">Login</button>
-        <hr>
-        <p>Don't have an account? <a href="Signup.php">SignUp</a></p>
-    </form>
+        <h2>Login to VibeOn</h2>
+        <form action="login_process.php" method="POST">
+            <label>Username or Email:</label>
+            <input type="text" name="login_id" required><br><br>
+
+            <label>Password:</label>
+            <input type="password" name="password" required><br><br>
+
+            <button type="submit">Login</button>
+            <hr>
+            <p>Don't have an account? <a href="Signup.php">SignUp</a></p>
+        </form>
+    </div>
 </body>
+
 </html>
