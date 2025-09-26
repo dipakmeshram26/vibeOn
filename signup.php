495a9d60218d5d794a689a1250d63b548a8f3869
Dipak meshram <meshramdm2@gmail.com>
2025-09-26 22:39:53 +0530

    update

--- signup.php
+++ signup.php
@@ -13,21 +13,21 @@
 
             <h2>Create Account</h2>
             <form action="signup_process.php" method="POST" enctype="multipart/form-data">
-                Full Name: <input type="text" name="full_name" required><br><br>
-                Username: <input type="text" name="username" required><br><br>
-                Email: <input type="email" name="email"><br><br>
-                Phone: <input type="text" name="phone"><br><br>
-                Password: <input type="password" name="password" required><br><br>
-                DOB: <input type="date" name="dob"><br><br>
+                  <input type="text" name="full_name" placeholder="Full Name: " required><br><br>
+                  <input type="text" name="username" placeholder="Username:" required><br><br>
+                  <input type="email" name="email" placeholder="Email:"><br><br>
+                  <input type="text" name="phone"placeholder="Phone:"><br><br>
+                  <input type="password" name="password" placeholder="Password:" required><br><br>
+                DOB: <input type="date" name="dob" placeholder=""><br><br>
                 Gender:
-                <select name="gender">
+                <select name="gender" placeholder="Gender">
                     <option value="">Select</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                     <option value="other">Other</option>
                 </select><br><br>
                 Profile Picture: <input type="file" name="profile_picture"><br><br>
-                Bio: <textarea name="bio"></textarea><br><br>
+                  <textarea name="bio"></textarea placeholder="Bio:"><br><br>
                 <button type="submit">Sign Up</button>
             </form>
         </div>
